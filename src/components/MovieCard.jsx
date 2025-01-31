8c79033a04a070c6ce86ae9e58e1a5f4713c18fe
Jovan Simonovic <jovan.simonovic02@gmail.com>
2025-02-01 00:13:58 +0100

    feature: implemented react-router

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { Link } from "react-router";
 
 const MovieCard = ({
-  movie: { title, vote_average, poster_path, release_date, original_language },
+  movie: { id, title, vote_average, poster_path, release_date, original_language },
 }) => {
   return (
     <div className="movie-card">
-      <img
-        src={
-          poster_path
-            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-            : "/no-movie.png"
-        }
-        alt={title}
-      />
+      <Link to={`/movie/${id}`}>
+        <img
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+              : "/no-movie.png"
+          }
+          alt={title}
+        />
+      </Link>
       <div className="mt-4">
         <h3>{title}</h3>
         <div className="content">
