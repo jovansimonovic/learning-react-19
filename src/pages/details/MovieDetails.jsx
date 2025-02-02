dd9ac71436800482f701606d5b256bf505b549fe
Jovan Simonovic <jovan.simonovic02@gmail.com>
2025-02-02 14:28:23 +0100

    fix: fix typos

--- src/pages/details/MovieDetails.jsx
+++ src/pages/details/MovieDetails.jsx
@@ -6,7 +6,7 @@ import { formatDate } from "../../utils";
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS = {
-  methor: "GET",
+  method: "GET",
   headers: {
     accept: "application/json",
     Authorization: `Bearer ${API_KEY}`,
--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -9,7 +9,7 @@ import { Link } from "react-router";
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS = {
-  methor: "GET",
+  method: "GET",
   headers: {
     accept: "application/json",
     Authorization: `Bearer ${API_KEY}`,
